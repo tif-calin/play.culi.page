3f2b5fdea29aa50c2534f45b6300edc41fff8173
culi🌸 <culitif@tuta.io>
2022-12-10 23:56:46 -0800

    adds fontello

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// TODO: add an external link symbol as an ::after that dynamically expands
 const ExternalLink = styled(({ children, ...props }: any) => <a target="_blank" rel="noopener noreferrer" {...props} >{children}</a>)`
   transition: color 0.1s;
   color: var(--color-link);
